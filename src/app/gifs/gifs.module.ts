2af22015227e05245d5c29d13995d8d800987599
Mr-Knister <cesar.gomez.maquera@gmail.com>
2023-06-19 09:37:43 -0500

    fin clase 8

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { SharedModule } from '../shared/shared.module';
+
+import { CardComponent } from './componentes/card/card.component';
+import { CardListComponent } from './componentes/card-list/card-list.component';
 import { HomePageComponent } from './pages/home/home.component';
 import { SearchBoxComponent } from './componentes/search-box/search-box.component';
-import { CardListComponent } from './componentes/card-list/card-list.component';
 
 
 
 @NgModule({
   declarations: [
+    CardComponent,
+    CardListComponent,
     HomePageComponent,
-    SearchBoxComponent,
-    CardListComponent
+    SearchBoxComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
       HomePageComponent
